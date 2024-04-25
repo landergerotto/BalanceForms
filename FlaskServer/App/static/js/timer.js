26d4ba2f66705d68bdc9e9bd0b3bb602855713f7
luizblank <luizrosacardoso@gmail.com>
2024-04-25 15:39:06 -0300

    Fixed more errors

--- FlaskServer/App/static/js/timer.js
+++ FlaskServer/App/static/js/timer.js
@@ -1,32 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('form');
-    const timerElement = document.getElementById('timer');
+const button = document.getElementById('button');
+const form = document.getElementById('form');
+const timerElement = document.getElementById('timer');
 
-    function Timer() {
-        let time = timerElement.innerHTML;
-        let hours = parseInt(time.split(':')[0]);
-        let minutes = parseInt(time.split(':')[1]);
-        let seconds = parseInt(time.split(':')[2]);
+button.onclick = () => {
+    button.disabled = true;
+    form.submit()
+}
 
-        if (hours == 0 && minutes == 0 && seconds == 0) {
-            form.submit();
-            return;
-        }
+function Timer() {
+    let time = timerElement.innerHTML;
+    let hours = parseInt(time.split(':')[0]);
+    let minutes = parseInt(time.split(':')[1]);
+    let seconds = parseInt(time.split(':')[2]);
 
-        if (seconds == 0) {
-            if (minutes == 0) {
-                hours -= 1;
-                minutes = 59;
-            } else {
-                minutes -= 1;
-            }
-            seconds = 59;
-        } else {
-            seconds -= 1;
-        }
-
-        timerElement.innerHTML = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+    if (hours == 0 && minutes == 0 && seconds == 0) {
+        form.submit();
+        return;
     }
 
-    setInterval(Timer, 1000);
-});
+    if (seconds == 0) {
+        if (minutes == 0) {
+            hours -= 1;
+            minutes = 59;
+        } else {
+            minutes -= 1;
+        }
+        seconds = 59;
+    } else {
+        seconds -= 1;
+    }
+
+    timerElement.innerHTML = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
+setInterval(Timer, 1000);
--- FlaskServer/App/static/js/verify.js
+++ FlaskServer/App/static/js/verify.js
@@ -3,12 +3,24 @@ var button = document.getElementById('button');
 var error = document.getElementById('error');
 
 time.onchange = () => {
-    if (isNaN(time.value))
-    {
+    if (isNaN(time.value)) {
         button.disabled = true;
+        error.innerHTML = "Preencha o campo apenas com números."
         error.style.display = 'block';
         return
+    } else {
+        button.disabled = false;
+        error.style.display = 'none';
     }
-    button.disabled = false;
-    error.style.display = 'none';
+
+    if (parseInt(time.value) > 180) {
+        button.disabled = true;
+        error.innerHTML = "Tempo de prova máximo de 180 minutos excedido."
+        error.style.display = 'block';
+        return
+    } else {
+        button.disabled = false;
+        error.style.display = 'none';
+        return
+    }
 }
