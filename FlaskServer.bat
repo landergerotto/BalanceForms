7601ada4ab6f8319d2b025cda0d9e8bb6d32ba9c
luizblank <luizrosacardoso@gmail.com>
2024-04-30 14:00:40 -0300

    Changed start server batch file

---
+++ FlaskServer.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd .\FlaskServer\
+
+if not exist ".venv/" echo Instalando as bibliotecas necessarias & python -m venv .venv/ & call .\.venv\Scripts\activate & pip install flask & pip install xlsxwriter
+
+start cmd.exe /c start "" http://127.0.0.1:5000/
+
+cls
+echo Manter o console aberto para que o servidor funcione apropriadamente.
+call .\.venv\Scripts\activate
+flask --app App run
